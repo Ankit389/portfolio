71a3d07022b6675b29d1ab0d661016ec5e25a69d
Ankit389 <ankitpankaj8044@gamil.com>
2024-09-07 22:45:03 +0530

    Updated App, Footer, and Computers components; removed Form component

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,9 @@
-// import { BrowserRouter } from "react-router-dom";
 
-// import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
-
-// const App = () => {
-//   return (
-//     <BrowserRouter>
-//       <div className='relative z-0 bg-primary'>
-//         <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
-//           <Navbar />
-//           <Hero />
-//         </div>
-//         <About />
-//         <Experience />
-//         <Tech />
-//         <Works />
-//         <Feedbacks />
-//         <div className='relative z-0'>
-//           <Contact />
-//           <StarsCanvas />
-//         </div>
-//       </div>
-//     </BrowserRouter>
-//   );
-// }
-
-// export default App;
-
-
-// import { BrowserRouter } from "react-router-dom";
-// import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
-// import Footer from "./components/Footer";  // Import the Footer component
-
-// const App = () => {
-//   return (
-//     <BrowserRouter>
-//       <div className='relative z-0 bg-primary'>
-//         <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
-//           <Navbar />
-//           <Hero />
-//         </div>
-//         <About />
-//         <Experience />
-//         <Tech />
-//         <Works />
-//         <Feedbacks />
-//         <div className='relative z-0'>
-//           <Contact />
-//           <StarsCanvas />
-//         </div>
-//         <Footer />  {/* Add Footer component here */}
-//       </div>
-//     </BrowserRouter>
-//   );
-// }
-
-// export default App;
 
 import { BrowserRouter } from "react-router-dom";
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 import Footer from "./components/Footer";  // Import Footer component
-// import Form from "./components/Form";      // Import Form component
+
 
 const App = () => {
   return (
@@ -75,10 +19,7 @@ const App = () => {
         <Works />
         <Feedbacks />
         
-        {/* Add Form component */}
-        {/* <div className='p-4'>
-          <Form />
-        </div> */}
+       
 
         <div className='relative z-0'>
           <Contact />
